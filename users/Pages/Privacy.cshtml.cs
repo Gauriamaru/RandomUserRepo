f557f1a7d862afeba1c9ed83fc8f929ac030dbc3
Gauriamaru <111969153+Gauriamaru@users.noreply.github.com>
2023-03-02 13:13:26 +0000

    Add files via upload

---
+++ users/Pages/Privacy.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text.Json.Serialization;
+
+namespace Users.Pages
+{
+	public class PrivacyModel : PageModel
+	{
+		private readonly ILogger<PrivacyModel> _logger;
+		public object Message { get; set; }
+		public PrivacyModel(ILogger<PrivacyModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+			
+		}
+	}
+}
